231b52eb973e77266312b500d2193bbd55580ceb
Carlos Garcia <Csharles@users.noreply.github.com>
2016-09-02 23:35:41 -0600

    Agregado el codigo para escribir lina a linea.

---
+++ usarSerializacion/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace usarSerializacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Crea un arreglo de tres lineas
+            string[] lineas = { "Primera Linea", "Segunda Linea", "Tercera Linea" };
+
+            // Establecemos la ruta donde se guardara el archivo y la carpeta.
+            string rutaEscritorio =
+                Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string folderTemporal = Path.Combine(rutaEscritorio, "temporal");
+            // Escribimos las lineas del arreglo en el archivo.
+            using (StreamWriter outputFile = new StreamWriter(folderTemporal + @"\ArchivoLineas.txt"))
+            {
+                foreach (string linea in lineas)
+                    outputFile.WriteLine(linea);
+            }
+        }
+    }
+}
