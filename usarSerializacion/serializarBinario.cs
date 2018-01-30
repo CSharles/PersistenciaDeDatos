d4de5162f3c317a254c64af7d60caf419565cd1a
Carlos Garcia <Csharles@users.noreply.github.com>
2018-01-30 12:26:28 -0600

    This is a minor change

--- usarSerializacion/serializarBinario.cs
+++ usarSerializacion/serializarBinario.cs
@@ -22,46 +22,46 @@ namespace usarSerializacion
 
                 if (opcion.Equals("1"))
                 {
-                    PedirDatos(hotel); 
+                    PedirDatos(hotel);
                 }
                 if (opcion.Equals("3"))
                 {
                     string ruta = CrearCarpeta();
-                    DeserializarDatos(ruta,hotel);
+                    DeserializarDatos(ruta, hotel);
                 }
 
                 if (opcion.Equals("2"))
                 {
-                    string ruta= CrearCarpeta();
+                    string ruta = CrearCarpeta();
                     SerializarDatos(ruta, hotel);
                 }
-                 
-            } while (!opcion.Equals("2")); 
-    }
+
+            } while (!opcion.Equals("2"));
+        }
 
         private static void DeserializarDatos(string ruta, Dictionary<int, string> hotel)
         {
-            using (FileStream fs = new FileStream(ruta+@"\hoteldata.bin", FileMode.Open))
+            using (FileStream fs = new FileStream(ruta + @"\hoteldata.bin", FileMode.Open))
             {
                 // Creación de instancia de BinaryFormatter para deserializar:
                 BinaryFormatter bf = new BinaryFormatter();
 
-                hotel = (Dictionary<int,string>)bf.Deserialize(fs);
+                hotel = (Dictionary<int, string>)bf.Deserialize(fs);
             }
 
             // Verificamos que el contenido ha sido correctamente deserializado:
             Console.WriteLine("Datos de hotel");
-            foreach (KeyValuePair<int,string> huesped in hotel)
+            foreach (KeyValuePair<int, string> huesped in hotel)
             {
                 Console.WriteLine("\nHabitacion:" + huesped.Key + "\nNombre:" + huesped.Value);
-                                  
+
             }
         }
 
-        private static void SerializarDatos(string ruta, Dictionary<int,string> hotel)
+        private static void SerializarDatos(string ruta, Dictionary<int, string> hotel)
         {
             // Creación de un flujo para escritura de los elementos del hashtable:
-            using (FileStream fs = new FileStream(ruta +@"\hoteldata.bin",FileMode.Append))
+            using (FileStream fs = new FileStream(ruta + @"\hoteldata.bin", FileMode.Append))
             {
                 // Creación instancia BinaryFormatter para serializar el diccionario.
                 BinaryFormatter bf = new BinaryFormatter();
@@ -83,7 +83,7 @@ namespace usarSerializacion
             return folderDatos;
         }
 
-        private static void PedirDatos(Dictionary<int,string>hotel)
+        private static void PedirDatos(Dictionary<int, string> hotel)
         {
             string opcion;
             do
